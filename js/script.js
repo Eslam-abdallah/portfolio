9a53fc45da16abb01fbeb16fe1f22c2316f23e1b
Eslam Abdallah Mohamed Elkomy <124469180+Eslam-abdallah@users.noreply.github.com>
2023-02-11 21:04:02 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle Nav
+$('#open-nav').click(function(){
+    $('#sidenav').toggleClass("sideshow");
+    $('#logo').toggle();
+    $('#open-nav').toggle();
+    $('.mainx').toggle();
+});
+//Change Icon
+$('#close-nav').click(function(){
+    $('#sidenav').toggleClass("sideshow");
+    $('#logo').toggle();
+    $('#open-nav').toggle();
+    $('.mainx').toggle();
+});
+
